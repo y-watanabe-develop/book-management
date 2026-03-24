7bc173df37e7f0c13fe438073b63ec68947a221e
y-watanabe-develop <y-watanabe@itime-works.com>
2026-03-24 20:45:59 +0900

    chore: initial setup

---
+++ src/test/kotlin/com/bookmanagement/book_management/BookManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bookmanagement.book_management
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
