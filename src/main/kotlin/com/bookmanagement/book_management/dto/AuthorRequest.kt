f4140d5d4f2fbc54fa095238ae30a076ec9b05ba
y-watanabe-develop <y-watanabe@itime-works.com>
2026-03-24 22:33:21 +0900

    chore: add validation dependency

--- src/main/kotlin/com/bookmanagement/book_management/dto/AuthorRequest.kt
+++ src/main/kotlin/com/bookmanagement/book_management/dto/AuthorRequest.kt
@@ -1,8 +1,12 @@
 package com.bookmanagement.book_management.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.PastOrPresent
 import java.time.LocalDate
 
 data class AuthorRequest(
+    @field:NotBlank
     val name: String,
+    @field:PastOrPresent
     val birthDate: LocalDate
 )
--- src/main/kotlin/com/bookmanagement/book_management/dto/BookRequest.kt
+++ src/main/kotlin/com/bookmanagement/book_management/dto/BookRequest.kt
@@ -1,7 +1,14 @@
 package com.bookmanagement.book_management.dto
 
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+
 data class BookRequest(
+    @field:NotBlank
     val title: String,
+    @field:Min(0)
     val price: Int,
+    @field:NotEmpty
     val authorIds: List<Long>
 )
