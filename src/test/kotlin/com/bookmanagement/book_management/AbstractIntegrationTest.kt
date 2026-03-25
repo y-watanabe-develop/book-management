c9d359e30407c073a53365163f99638469c58dbf
y-watanabe-develop <y-watanabe@itime-works.com>
2026-03-25 19:12:02 +0900

    test: add @Transactional to AbstractIntegrationTest for test isolation

--- src/test/kotlin/com/bookmanagement/book_management/AbstractIntegrationTest.kt
+++ src/test/kotlin/com/bookmanagement/book_management/AbstractIntegrationTest.kt
@@ -3,9 +3,11 @@ package com.bookmanagement.book_management
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
+import org.springframework.transaction.annotation.Transactional
 import org.testcontainers.containers.PostgreSQLContainer
 
 @SpringBootTest
+@Transactional
 abstract class AbstractIntegrationTest {
 
     companion object {
